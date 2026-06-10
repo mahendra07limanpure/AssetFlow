dd5b1e50afbb455a59c664b419d6e8398ec0528c
mahendra07limanpure <shashanklimanpure07@gmail.com>
2026-06-10 13:41:48 +0530

    user schema set up and jwt services

--- server/server.js
+++ server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const connectDB = require("./config/db");
+connectDB();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+const authRoutes = require("./routes/authRoutes");
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
   res.send("AssetFlow API Running");
